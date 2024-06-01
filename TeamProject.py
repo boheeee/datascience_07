f2b5bb88abef86f805d8d049ad5329ad6dfc3f4c
LJRight <111737816+LJRight@users.noreply.github.com>
2024-06-02 03:52:47 +0900

    code update

--- TeamProject.py
+++ TeamProject.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, OneHotEncoder, LabelEncoder
@@ -13,13 +14,13 @@ from sklearn.neighbors import KNeighborsClassifier
 
 """
     Functions to fill in missing values in data. 
-    1. delete unnecessary columns: 'Date' 
+    1. delete unnecessary columns: 'Date'
     2. create a categorical data attribute: 'pH' (float -> categorical)
     3. fill in the missing values 
         1) Replace with the mean value for real columns
         2) Replace with the mode value for categorical columns
         3) Predict with a regression model for the column 'DissolvedOxygen (mg/L)', which has many missing values:
-           The categorical column 'pH' was temporarily encoded with one-hot encoding.
+           The categorical column 'pH' was temporarily encoded with one-hot encoding for regression
 """
 
 
@@ -51,10 +52,12 @@ def Fill_Missing_Value(df):
         else:
             return 'Alkaline'
 
-    # 범주형 데이터 pH 열에 대하여 더미 변수 생성(one-hot encoding) 함수 / pH 열은 지우고 해당 열을 one-hot encoding 방식으로 변환한다.
-    # 이리하여 결측값이 많은 DissolvedOxygen (mg/L) (용존산소량) 열의 값을 회귀모델 예측치로 대체할 때 pH 열 또한 입력으로 넣을 수 있다.
-    # Create a dummy variable for the pH column of categorical data (one-hot encoding) function
-
+    """
+        Create a dummy variable for the categorical data pH column (one-hot encoding) function
+        Delete the pH column and convert the column to one-hot encoding.
+        This way, when replacing the values in the DissolvedOxygen (mg/L) column with regression model predictions, 
+        which have many missing values, the pH column can also be included as input.
+    """
     def create_dummy_variables(df):
         dummies = pd.get_dummies(df['pH'], prefix='pH')
         df = df.drop('pH', axis=1)
@@ -79,8 +82,7 @@ def Fill_Missing_Value(df):
     print("\nThe number of each category in the 'pH' column:")
     print(ph_value_counts)
 
-    # selec_dtypes 설명 필요
-    # DissolvedOxygen (mg/L) 열을 제외하고 평균값으로 결측값 대체
+    # Replace missing values with mean values, except for the 'DissolvedOxygen (mg/L)' column
     numeric_columns = df.select_dtypes(include='number').drop(
         'DissolvedOxygen (mg/L)', axis=1)
     df[numeric_columns.columns] = numeric_columns.fillna(
@@ -90,32 +92,32 @@ def Fill_Missing_Value(df):
     mode_value = df['pH'].mode()[0]
     df['pH'].fillna(mode_value, inplace=True)
 
-    # pH 열을 더미변수 (one-hot encoding) 방식으로 변환하여 용존 산소량 열을 제외한 모든 열을 사용하여 용존 산소량 결측치 값을 예측할 수 있다. 아래는 그 과정
+    # Convert a column of categorical data to a dummy variable (= One-hot encoding)
     df_dummy = create_dummy_variables(df)
 
-    # 전체 데이터에서 용존 산소량 값이 비어있지 않은 행들을 선택하여 학습 데이터 사용한다.
+    # Select rows with non-empty 'DissolvedOxygen (mg/L)' values from the entire data to use as training data.
     df_train = df_dummy[df['DissolvedOxygen (mg/L)'].notnull()]
-    # 마찬가지로 이번엔 비어있는 부분을 찾아서 해당 부분은 예측치 값을 채울 때 사용한다.
+    # Similarly, this time we look for empty parts and use them to fill in the predicted value.
     df_missing = df_dummy[df_dummy['DissolvedOxygen (mg/L)'].isnull()]
 
-    # 학습 데이터를 독립변수(입력)과 종속변수(타겟/예측 대상) 으로 구분
+    # Divide training data into independent variables (inputs) and dependent variables (targets/predictions)
     X_train = df_train.drop(columns='DissolvedOxygen (mg/L)')
     y_train = df_train['DissolvedOxygen (mg/L)']
 
-    # 선형 회귀 모델 학습
+    # Train a linear regression model
     regressor = LinearRegression()
     regressor.fit(X_train, y_train)
 
-    # 용존 산소량 값이 비어있는 데이터에서 해당 열 삭제(타겟/예측 대상이기 때문)
+    # Delete the column from the data where the 'DissolvedOxygen (mg/L)' value is empty (because it is targeted/predicted)
     X_missing = df_missing.drop(columns='DissolvedOxygen (mg/L)')
-    # 용존 산소량 결측치 예측/추정
+    # Predicting 'DissolvedOxygen (mg/L)' missing values
     y_missing_pred = regressor.predict(X_missing)
 
-    # 원본 데이터에서 결측치를 예측치로 대체
+    # Replacing missing values in original data with predictions
     df.loc[df['DissolvedOxygen (mg/L)'].isnull(),
            'DissolvedOxygen (mg/L)'] = y_missing_pred
 
-    # 결측값을 처리한 깨끗한 데이터 반환
+    # Return clean data with missing values handled
     return df
 
 
@@ -129,8 +131,8 @@ def Fill_Missing_Value(df):
 
 def Create_Data_Preprocessing_Combinations(df_clean):
 
-    # TermProject 자료(p27)에 있던 Mean normalization 스케일러를 직접 선언
-    class Meannormalization(BaseEstimator, TransformerMixin):
+    # Defining the Mean normalisation class
+    class Meannormalization():
         def fit(self, X, y=None):
             self.mean_ = np.mean(X, axis=0)
             self.min_ = np.min(X, axis=0)
@@ -142,70 +144,94 @@ def Create_Data_Preprocessing_Combinations(df_clean):
 
         def fit_transform(self, X, y=None):
             return self.fit(X, y).transform(X)
-    # 숫자형 열의 이름
+
+    # Names for numeric columns
     numeric_columns = df_clean.select_dtypes(
         include=['float64', 'int64']).columns
-    # 범주형 열의 이름
+    # Names for categorical columns
     categorical_column = 'pH'
 
-    # 전처리 조합에 사용될 스케일러들을 딕셔너리리로 생성
+    # Create a dictionary of scalers to be used in preprocessing combinations
     scalers = {
         'min-max': MinMaxScaler(),
         'standard': StandardScaler(),
         'robust': RobustScaler(),
         'mean': Meannormalization()
     }
-    # 전처리 조합에 사용될 인코더들을 딕셔너리로 생성
+    # Create a dictionary of encoders to be used in preprocessing combinations
     encoders = {
-        # sparse_output=False 이면 희소행렬로 출력하지 않게 한다. 기본값은 True 이며 이는 3개의 범주에 대하여 인코딩 시 하나의 열, (0,0,1) 이런 식의 출력물을 내놓는 것을 의미하며
-        # False 일 시 각각을 새로운 열로 하여 0 | 0 | 1 로 생성한다. 여기서는 익숙한 방식으로 좀 더 다루기 쉽게하기 위해 값을 False 로 지정하여, pH 열에 대해 인코딩 하면 새로운 열 3개를 추가하도록 한다.
+        """
+            If sparse_output=False, do not output to sparse matrix. 
+            The default is True, which means that the encoding will output one column for each of the three categories, (0,0,1), and so on.
+            If False, it will generate 0 | 0 | 1 with each as a new column. 
+            Here, to make it more tractable in a familiar way, we'll specify the value as False, 
+            so that encoding for the pH column will add three new columns.
+        """
         'one-hot': OneHotEncoder(sparse_output=False),
         'label': LabelEncoder()
     }
-
-    # 각 전처리 방법의 조합에 대한 데이터 프레임을 저장하기 위한 딕셔너리. key 값은 (스케일러 이름, 인코더 이름) 쌍의 튜플로 이루어지고 , value 는 DataFrame 이다.
+    """
+        A dictionary for storing data frames for each combination of preprocessing methods. 
+        The key values are tuples of (scaler name, encoder name) pairs, and the value is a DataFrame.
+    """
     df_combinations = {}
 
     for scaler_name, scaler in scalers.items():
-        # 범주형 데이터 'pH' 열을 제외한 나머지 열을 선택하여 복사
+        # Select and copy the remaining columns except the categorical data 'pH' column
         scaled_df = df_clean.copy().drop(columns=[categorical_column])
-        # 해당 열을 선택된 스케일러로 스케일링 한다
+        # Scales the column by the selected scaler
         scaled_df[numeric_columns] = scaler.fit_transform(
             scaled_df[numeric_columns])
         for encoder_name, encoder in encoders.items():
-            # 범주형 데이터 'pH' 열을 제외한 나머지 열을 drop 한 뒤 복사, 즉 'pH' 열만 선택
+            # Categorical data drop and copy the remaining columns except for the 'pH' column, i.e. select only the 'pH' column
             encoded_df = df_clean.copy().drop(columns=numeric_columns)
-            # encoder 가 one-hot encoder 라면 pH 농도에 범주 3개에 대한 새로운 열 3개가 새로 생성되어야 한다.
-            # get_feature_names_out 에 대해 조사해서 설명해주세요. 저도 잘 모르고 썼어요.
+            # If the encoder is a one-hot encoder, then the pH concentration should have three new columns for the three categories.
             if encoder_name == 'one-hot':
                 encoded_df = pd.DataFrame(encoder.fit_transform(
                     encoded_df), columns=encoder.get_feature_names_out([categorical_column]))
-            # label encoder 일 경우 기존 'pH' 열의 값을 인코딩 된 값으로 변경
+            
+            # Change the value of the existing 'pH' column to the encoded value if label encoder
             elif encoder_name == 'label':
                 encoded_df[categorical_column] = encoder.fit_transform(
                     encoded_df[categorical_column])
-            
-            # 스케일링, 인코딩 한 데이터를 합쳐서 하나의 DataFrame 으로 만듬
+
+            # Scaling and combining encoded data into a single DataFrame
             combined_df = pd.concat([scaled_df, encoded_df], axis=1)
-            # 해당 값을 딕셔너리에 저장. ex) min-max scaler 와 label encoding 방식의 조합으로 생성된 데이터에 접근하기 위해서는 df_combinations[('min-max', 'label')]
             df_combinations[(scaler_name, encoder_name)] = combined_df
 
-    # 조합으로 생성 된 8개의 value 를 갖는 딕셔너리 반환
+    # Return a dictionary with 8 values generated by the combination
     return df_combinations
+
+
+def Visualise_data_distribution(df):
+    plt.figure(figsize=(15, 10))
+    for i, col in enumerate(df.columns, start=1):
+        if col == 'pH':
+            pH_counts = df[col].value_counts()
+            plt.subplot(3, 3, i)
+            plt.pie(pH_counts, labels=pH_counts.index, autopct='%1.1f%%')
+            plt.title(f"Distribution of {col}")
+        else:
+            plt.subplot(3, 3, i)
+            sns.kdeplot(df[col], fill=True)
+            plt.title(f"Distribution of {col}")
+    plt.tight_layout()
+    plt.show()
 
 
 df = pd.read_csv("data/waterquality.csv")
 df_clean = Fill_Missing_Value(df)
+Visualise_data_distribution(df_clean)
 df_combinations = Create_Data_Preprocessing_Combinations(df_clean)
 
-# 모든 데이터 조합 출력
+# Output all data combinations
 for key, value in df_combinations.items():
     print(f"{key}")
-    print(value.head())
+    print(value.head(), "\n")
 
 
 # 사용할 모델과 hyperparameter tuning 에 사용될 parameter 를 딕셔너리(grid) 형식으로 지정
-# 아래의 parameter 들에 대해 조사하여 문서 작성 시 추가해 주세요. 예를 들면 C 는 로지스틱스 회귀 모델에서 규제 정도를 나타내는 값입니다. 
+# 아래의 parameter 들에 대해 조사하여 문서 작성 시 추가해 주세요. 예를 들면 C 는 로지스틱스 회귀 모델에서 규제 정도를 나타내는 값입니다.
 # 특히 여기서는 사용되지 않았지만 LogisticRegression() 의 parameter 로 penalty 와 solver 가 있는데 특정 solver 는 특정 penalty 에서만 유효하게 동작한다고 하는데 왜 그런지 간단하게 조사하면 좋을 거 같아요
 knn_param_grid = {
     'n_neighbors': np.arange(1, 29),
@@ -233,8 +259,7 @@ logistic_gscv = GridSearchCV(
 X_columns = df_clean.select_dtypes(
     include=['float64', 'int64']).columns
 y_columns_label = 'pH'
-y_columns_one_hot = df_combinations[(
-    'min-max', 'one-hot')].columns.tolist()[-3:]
+y_columns_one_hot = ['pH_Acidic',  'pH_Alkaline',  'pH_Neutral']
 
 best_combinations_score = {}
 
@@ -248,10 +273,6 @@ for (scaler_name, encoder_name), df in df_combinations.items():
 
     knn_gscv.fit(X_train, y_train)
     tree_gscv.fit(X_train, y_train)
-    if encoder_name == 'one-hot':
-        logistic_gscv.fit(X_train, np.argmax(y_train.values, axis=1))
-    else:
-        logistic_gscv.fit(X_train, y_train)
     logistic_gscv.fit(X_train, y_train if encoder_name !=
                       'one-hot' else np.argmax(y_train.values, axis=1))
 
